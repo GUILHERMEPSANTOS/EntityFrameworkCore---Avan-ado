63fbc20f25664dc321d00ae0b869766e267235aa
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-01-04 10:42:04 -0300

    feat: implementação de consultas atraves de storerd procedures

--- EFCoreAvancado/Program.cs
+++ EFCoreAvancado/Program.cs
@@ -1,7 +1,6 @@
 using EFCoreAvancado.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace EFCoreAvancado
 {
@@ -9,11 +8,56 @@ namespace EFCoreAvancado
     {
         public static void Main()
         {
-            CriarStoredProcedure();
+            // CriarStoredProcedure();
+            // InserirDadosViaProcedure();
+            // CriarStoredProcedureDeConsulta();
+            ConsultaViaProcedure();
         }
 
+        public static void ConsultaViaProcedure()
+        {
+            {
 
-        public static void CriarStoredProcedure() 
+                var db = new ApplicationDbContext();
+
+                var dep = new SqlParameter("@dep", "Departamento");
+
+                var departamentos = db.Departamentos
+                    .FromSqlRaw("EXEC GetDepartamento @dep", dep)
+                    .ToList();
+
+                foreach (var departamento in departamentos)
+                {
+                    Console.WriteLine($"Deparatamento Descrição: {departamento.Descricao}");
+                }
+            }
+        }
+        public static void CriarStoredProcedureDeConsulta()
+        {
+
+            var db = new ApplicationDbContext();
+
+            var scritpStoredProcedure = @"
+                CREATE OR ALTER PROCEDURE GetDepartamento @Descricao VARCHAR(50)
+                AS
+                BEGIN
+                    SELECT *
+                    FROM Departamentos 
+                    WHERE Descricao Like @Descricao + '%'
+                END
+            ";
+
+            db.Database.ExecuteSqlRaw(scritpStoredProcedure);
+        }
+
+        public static void InserirDadosViaProcedure()
+        {
+            var db = new ApplicationDbContext();
+
+            db.Database.ExecuteSqlRaw("EXEC CriarDepartamento @p0, @p1", "Departamento Via Proc", true);
+        }
+
+        public static void CriarStoredProcedure()
         {
 
             var db = new ApplicationDbContext();
